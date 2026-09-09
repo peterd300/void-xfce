0698f4041eb0eb8058de077b7043e0273939302d
Peter <peterd300@gmail.com>
2026-09-09 15:19:24 +0200

    Added script directory for several scripts

---
+++ scripts/doom.sh
@@ -0,0 +1,20 @@
+# Install gzdoom and several doom wad files
+# Back to the old days :)
+# see https://github.com/Akbar30Bill/DOOM_wads/tree/master for more files
+
+
+!#/usr/bin/env bash
+
+# install emulator
+sudo xbps-install -Suy gzdoom
+mkdir -p ~/.config/gzdoom
+cp ./scripts/gzdoom.ini  ~/config/gzdoom/.
+
+
+cd ~
+mkdir -p ~/games
+cd games
+
+wget https://raw.githubusercontent.com/Akbar30Bill/DOOM_wads/refs/heads/master/doom.wad
+wget https://raw.githubusercontent.com/Akbar30Bill/DOOM_wads/refs/heads/master/doom2.wad
+# echo "Start gzdoom from this directory ~/games/."
