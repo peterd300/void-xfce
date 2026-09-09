c3b9dddcc11801b4f629d9ef863f70abbdaf2c43
Peter <peterd300@gmail.com>
2026-09-09 20:15:33 +0200

    install script for zig 0.16

---
+++ scripts/zig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Enter the sudo password:"
+sudo -v
+
+mkdir -p  ~/temp
+cd ~/temp/
+
+wget https://ziglang.org/download/0.16.0/zig-x86_64-linux-0.16.0.tar.xz
+tar -xvf zig*.tar.xz
+cd zig-x86_64-linux-0.16.0/
+bat README.md
+
+sudo mkdir /usr/local/bin/doc
+sudo mkdir -p /usr/local/lib/zig
+
+sudo mv zig /usr/local/bin/.
+sudo mv doc/* /usr/local/bin/doc/.
+sudo mv lib/*  /usr/local/lib/zig/.
+
+cd ~
+
+rm -rf ~/temp
